cbc8b49c9ce34194811cd9fa9321b4e6d6fb3a73
Sang HT <huynhtsang2004@gmail.com>
2024-06-24 19:38:46 +0700

    fix setting

---
+++ JS/Data/slideData.js
@@ -0,0 +1,50 @@
+export const slideData = [
+    {
+        img:" https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/a/2/5/b/a25ba756e50de822f8d026b00ce99b03.jpg",
+        title: "Sao Không Ở Lại",
+        actor: "Lưu Hưng",
+        date: "19.6.2024"
+    },
+    {
+        img:"https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/5/0/9/1/509166020c61b2b83228a74534e590ef.jpg",
+        title: "Đổ Sông Đổ Biển",
+        actor: "Khang Việt",
+        date: "16.6.2024"
+    },
+    {
+        img:"https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/b/8/9/0/b8903b1ad10d36f5eb44c8f25cb90b49.jpg",
+        title: "Ngày Mai Tôi Sẽ Viết Sách",
+        actor: "Quang Hà",
+        date: "14.6.2024"
+    },
+    {
+        img:"https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/8/3/a/7/83a7a35a5ed4b3b658c7fb39164c0cd4.jpg",
+        title: "Cái Tên Khắc Sâu Trong Tim",
+        actor: "Hà Nhi, Quốc Thiên",
+        date: "21.6.2024"
+    },
+    {
+        img:"https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/4/6/3/e/463efdaa10eb3142d3459d40ad15c8bb.jpg",
+        title: "Tự Em Sai",
+        actor: "Linh Hương Luz, IC Music",
+        date: "13.6.2024"
+    },
+    {
+        img:"https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/4/1/d/d/41dd1e486daa839514003c7d133f1a11.jpg",
+        title: "Lệ Phí Cuộc Đời",
+        actor: "Cao Thái Sơn, Đông Thiên Trúc",
+        date: "19.6.2024"
+    },
+    {
+        img:"https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/2/8/2/f/282f81752cf27269689abc25fecbcadb.jpg",
+        title: "Bài Hát Dành Cho Con",
+        actor: "Hồ Ngọc Hà, Nguyễn Hồng Thuận",
+        date: "21.6.2024"
+    },
+    {
+        img:"https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/1/9/b/1/19b1710b5010ea6efc3703f3cd3b1adb.jpg",
+        title: "Hẳn Là Yêu",
+        actor: "Keyo",
+        date: "17.6.2024"
+    },
+]
---
+++ JS/index.js
@@ -0,0 +1,7 @@
+import { handleRenderItemsSlide } from "./HandleRender/handleRenderItemsSlide.js";
+import { handleSearchInput } from "./HandleSearch/handleSearchInput.js";
+import { handleSlideRank } from "./HandleSlide/handleSlideRank.js";
+import { slideData } from './Data/slideData.js';
+handleRenderItemsSlide(slideData);
+handleSearchInput();
+handleSlideRank();
