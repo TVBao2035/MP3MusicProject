66dcf7e24d23aa071f56e6668381bed16b012583
Sang HT <huynhtsang2004@gmail.com>
2024-06-26 17:14:07 +0700

    signIn

---
+++ JS/HandleForm/handleSignInForm.js
@@ -0,0 +1,41 @@
+
+export const handleSignInForm = () => {
+    
+    function showError(input, message) {
+        let parent = input.parentElement;
+        let small = parent.querySelector('small');
+        // Thêm text thông báo lỗi vào small bỏ trống
+        small.innerText = message;
+        small.style.color = 'rgb(206, 30, 30)';
+    }
+
+    function showSuccess(input) {
+        let parent = input.parentElement;
+        let small = parent.querySelector('small');
+        // Nếu nhập thì small trống
+        small.innerText = '';
+    }
+
+    function checkEmptyError(listInput) {
+        let isEmptyError = false;
+        listInput.forEach(input => {
+            if (!input.value.trim()) {
+                isEmptyError = true;
+                showError(input, "Không được để trống");
+            }
+            else{
+                showSuccess(input)
+            }
+        });
+        return isEmptyError
+    }
+    var username = document.querySelector('.login_username');
+    var password = document.querySelector('.login_password');
+    var form = document.querySelector('.signIn_form');
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault(); // Không load trang
+
+        let isEmptyError = checkEmptyError([username, password]); // Truyền mảng các input
+    });
+}
--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,10 @@
+
 import { handleRenderItemsSlide } from "./HandleRender/handleRenderItemsSlide.js";
 import { handleSearchInput } from "./HandleSearch/handleSearchInput.js";
 import { handleSlideRank } from "./HandleSlide/handleSlideRank.js";
 import { slideData } from './Data/slideData.js';
+import { handleSignInForm } from "./HandleForm/handleSignInForm.js";
+handleSignInForm();
 handleRenderItemsSlide(slideData);
 handleSearchInput();
 handleSlideRank();
