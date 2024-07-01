91f57511b77097c7c80e424f0d3ff194c28f8614
Sang HT <huynhtsang2004@gmail.com>
2024-07-01 20:11:56 +0700

    signin

--- JS/HandleForm/handleSignInForm.js
+++ JS/HandleForm/handleSignInForm.js
@@ -1,46 +1,124 @@
+function Validator(options) {
+    var formElement = document.querySelector(options.form);
 
-    function showError(input, message) {
-        let parent = input.parentElement;
-        let small = parent.querySelector('small');
-        // Thêm text thông báo lỗi vào small bỏ trống
-        small.innerText = message;
-        small.style.color = 'rgb(206, 30, 30)';
+    var selectorRules = {};
+
+    // Hàm thực hiện validate
+    function validate(inputElement, rule) {
+        var errorMessage;
+        var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+
+        // Lấy ra các rules của selector
+        var rules = selectorRules[rule.selector];
+
+        // Lặp qua từng rule và kiểm tra
+        // Nếu có lỗi thì dừng việc kiểm tra
+        for (var i = 0; i < rules.length; i++) {
+            errorMessage = rules[i](inputElement.value);
+            if (errorMessage) break;
+        }
+
+        if (errorMessage) {
+            errorElement.innerText = errorMessage;
+            inputElement.parentElement.classList.add('error');
+            inputElement.parentElement.classList.remove('success');
+        } else {
+            errorElement.innerText = '';
+            inputElement.parentElement.classList.remove('error');
+            inputElement.parentElement.classList.add('success');
+        }
+
+        return !errorMessage;
     }
 
-    function showSuccess(input) {
-        let parent = input.parentElement;
-        let small = parent.querySelector('small');
-        // Nếu nhập thì small trống
-        small.innerText = '';
-    }
+    if (formElement) {
+        formElement.onsubmit = function (e) {
+            e.preventDefault();
 
-    function checkEmptyError(listInput) {
-        let isEmptyError = false;
-        listInput.forEach(input => {
-            if (!input.value.trim()) {
-                isEmptyError = true;
-                showError(input, "Không được để trống");
+            var isFormValid = true;
+
+            // Lặp qua từng rule và validate
+            options.rules.forEach(function (rule) {
+                var inputElement = formElement.querySelector(rule.selector);
+                var isValid = validate(inputElement, rule);
+                if (!isValid) {
+                    isFormValid = false;
+                }
+            });
+
+            if (isFormValid) {
+                // Trường hợp submit với JavaScript
+                if (typeof options.onSubmit === 'function') {
+                    var enableInputs = formElement.querySelectorAll('[name]');
+                    var formValues = Array.from(enableInputs).reduce(function (values, input) {
+                        values[input.name] = input.value;
+                        return values;
+                    }, {});
+
+                    options.onSubmit(formValues);
+                }
+                // Trường hợp submit với hành vi mặc định
+                else {
+                    formElement.submit();
+                }
             }
-            else{
-                showSuccess(input)
+        }
+
+        // Lặp qua mỗi rule và xử lý (lắng nghe sự kiện blur, input, ...)
+        options.rules.forEach(function (rule) {
+
+            // Lưu lại các rules cho mỗi input
+            if (Array.isArray(selectorRules[rule.selector])) {
+                selectorRules[rule.selector].push(rule.test);
+            } else {
+                selectorRules[rule.selector] = [rule.test];
+            }
+
+            var inputElement = formElement.querySelector(rule.selector);
+
+            if (inputElement) {
+                // Xử lý trường hợp blur khỏi input
+                inputElement.onblur = function () {
+                    validate(inputElement, rule);
+                }
+
+                // Xử lý mỗi khi người dùng nhập vào input
+                inputElement.oninput = function () {
+                    var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+                    errorElement.innerText = '';
+                    inputElement.parentElement.classList.remove('error');
+                    inputElement.parentElement.classList.add('success');
+                }
             }
         });
-        return isEmptyError
     }
-    
-    var username = document.querySelector('.login_username');
-    var password = document.querySelector('.login_password');
-    var form = document.querySelector('.signIn_form');
+}
 
-    form.addEventListener('submit', function(e) {
-        e.preventDefault(); // Không load trang
+// Định nghĩa các rules
+Validator.isRequired = function (selector, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value.trim() ? undefined : message;
+        }
+    };
+}
 
-    });
+Validator.isEmail = function (selector, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return regex.test(value) ? undefined : message;
+        }
+    };
+}
 
-    var btnSumit = document.querySelector('.btn--Submit');
-
-    
-    btnSumit.onclick = function (e){
-        e.preventDefault();
-        checkEmptyError([username, password]);
-    }
+Validator.minLength = function (selector, min, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value.length >= min ? undefined : message;
+        }
+    };
+}
